84272fc0d64cfcd4a6e639ec0a3c56fb8504227f
Ronny Worm <r-worm@gmx.de>
2015-10-11 12:12:06 +0200

    Ist nun ausgereifter

--- pythonguard.sh
+++ pythonguard.sh
@@ -8,21 +8,28 @@ if [ $# -lt 2 ]
   then
 	cat << EOF
 Wenn in diesem Verzeichnis sleep_until_modified.py liegt, kann jedes Mal ein Skript ausgeführt werden, wenn sich eine angegebene Datei ändert. Dies wiederholt sich bis $scriptfilename beendet wird.
-Um das Programm zu beenden, muss zwei Mal Control-C gedrückt werden, sonst wird nur Python beendet.
+Um das Programm zu beenden, muss zwei Mal Control-C gedrückt werden, sonst wird nur Python beendet. Bei Beendigung dieses Skripts wird zwangsweise das übergebene Skript nochmal ausgeführt.
+Wichtig: Immer vollständige Pfade eingeben!
 
-Beispielaufruf: ./$scriptfilename /skript.sh /sich_aendernde_Datei.txt [Parameter_an_skript]
+Beispielaufruf: ./$scriptfilename /complete_path/skript.sh /complete_path/sich_aendernde_Datei.txt [Parameter_an_skript]
 EOF
 	exit
 fi
 
+script_directory=$(dirname "$1")
+my_directory=$(pwd)
+
+echo "Python Guard wurde gestartet und wartet auf Änderungen an $2 ..."
+cd "$script_directory"
+
 while [ 1 ]
 do
-    python sleep_until_modified.py "$2"
+    python "$my_directory/sleep_until_modified.py" "$2"
     if [ "$#" -eq 3 ]
   		then
-  			./"$1" "$3"
+  			"$1" "$3"
   		else
-    		./"$1"
+    		"$1"
   	fi
   	sleep 1
   	#Während Sleep kann das Programm beendet werden mit Control-C
